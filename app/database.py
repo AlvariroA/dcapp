a66407dcedff4a740ed10192717339ae4869fe98
david_avilac <adavila59@ucatolica.edu.co>
2022-06-16 07:12:57 -0500

    make it better

--- app/database.py
+++ app/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/dbprueba"
+SQLALCHEMY_DATABASE_URL = "postgresql://ueniqeyafvvivh:3e386f00a4e558cfbaabb77da612878e70392332947b90eea3d3328b2b528c03@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d23cemb0go55pp"
  
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import Response, Request, Depends, status, Form, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.encoders import jsonable_encoder
-
+from fastapi.security import OAuth2PasswordBearer
 
 #starlette
 from starlette.responses import HTMLResponse
@@ -26,6 +26,8 @@ from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 from routes.auth import auth_routes
 from passlib.context import CryptContext
+from app.functions_jwt import write_token
+
 
 ###routes
 from routes.dcapp_users import dcapp_users
@@ -50,9 +52,6 @@ def get_hashed_password(plain_password):
 def verify_password(plain_password, hashed_password):
     return pwd_ctx.verify(plain_password, hashed_password)
 
-
-
-
 def directory_is_ready():
     os.makedirs(os.getcwd()+"/img", exist_ok=True)
     return os.getcwd()+"/img/"
@@ -62,19 +61,19 @@ def directory_is_ready():
 ###Conexion con la base de datos
 
         
-"""@dcapp_users.post("/home")
-def root(request: Request):
-    return templates.TemplateResponse("usuario/home.html", {"request": request, "title": "Home"})"""
-
-
 
 ####Crud Basico
-@app.get('/usuarios/',response_model=List[schemas.User])
+@app.get('/usuarios/',
+         response_model=List[schemas.User],
+         tags=["Pruebas"],
+         summary="Lista los usuarios.")
 def show_users(db:Session=Depends(db.get_db)):
     usuarios = db.query(models.User).all()
     return usuarios
 
-@app.post('/usuarios/',response_model=schemas.User)
+@app.post('/usuarios/',
+          response_model=schemas.User,tags=["Pruebas"],
+          summary="Registro de un usuario.")
 def create_users(entrada:schemas.User, db:Session=Depends(db.get_db)):
     usuario = models.User(id = int(entrada.id), name = entrada.name, email = entrada.email)
     db.add(usuario)
@@ -82,7 +81,10 @@ def create_users(entrada:schemas.User, db:Session=Depends(db.get_db)):
     db.refresh(usuario)
     return usuario
 
-@app.put('/usuarios/{usuario_id}',response_model=schemas.User)
+@app.put('/usuarios/{usuario_id}',
+         response_model=schemas.User,
+         tags=["Pruebas"],
+         summary="Actualiza un usuario")
 def create_users(usuario_id:int, entrada:schemas.UserUpdate, db:Session=Depends(db.get_db)):
     usuario = db.query(models.User).filter_by(id=usuario_id).first()
     usuario.name=entrada.name
@@ -90,7 +92,10 @@ def create_users(usuario_id:int, entrada:schemas.UserUpdate, db:Session=Depends(
     db.refresh(usuario)
     return usuario
 
-@app.delete('/usuarios/{usuario_id}',response_model=schemas.Respuesta)
+@app.delete('/usuarios/{usuario_id}',
+            response_model=schemas.Respuesta, 
+            tags=["Pruebas"],
+            summary="Elimina a un usuarios.")
 def delete_users(usuario_id:int,db:Session=Depends(db.get_db)):
     usuario = db.query(models.User).filter_by(id=usuario_id).first()
     db.delete(usuario)
@@ -101,23 +106,23 @@ def delete_users(usuario_id:int,db:Session=Depends(db.get_db)):
 
 ####Response in HTMLResponse
 
-@app.get("/")
+@app.get("/", 
+          tags=["Home"],
+          summary="Pestaña de bienvenida.")
 def root(request: Request):
     return templates.TemplateResponse("inicio/index.html", {"request": request, "title": "Home"})
 
 
-@app.get("/signin")
+@app.get("/signin", 
+          tags=["Usuario"],
+          summary="Muestra la pestaña de signin.")
 def root(request: Request):
     return templates.TemplateResponse("inicio/signin.html", {"request": request, "title": "Home"})
 
 
-@app.get("/login")
-def root(request: Request):
-    return templates.TemplateResponse("inicio/login.html", {"request": request, "title": "Home"})
-
-
-
-@app.post("/signin")
+@app.post("/signin", 
+          tags=["Usuario"],
+          summary="realiza el registro de un usuario en BD.")
 def register(request: Request,
              username:str=Form(...),
              edad:str=Form(...),
@@ -154,8 +159,39 @@ def register(request: Request,
                                           status_code=status.HTTP_400_BAD_REQUEST)
 
 
+@app.get("/login",
+         tags=["Usuario"],
+         summary="Muestra la ventana de login.")
+def root(request: Request):
+    return templates.TemplateResponse("inicio/login.html", {"request": request, "title": "Home"})
 
 
+@app.post("/login", 
+          tags=["Usuario"],
+          summary="Valida si un usuario esta en la BD.")
+def login(request:Request, 
+          username:str=Form(...),
+          password:str=Form(...),
+          db: Session=Depends(db.get_db)):
+    user = jsonable_encoder(crud.get_user_by_username(db, username))
+    if not user:
+        return templates.TemplateResponse("inicio/login.html", {"request": request,
+                                                         "title": "Login"
+                                                         }, status_code=status.HTTP_401_UNAUTHORIZED)
+    if verify_password(password, user['hashed_password']):
+        write_token(user)
+        resp = RedirectResponse("/home", status_code=status.HTTP_302_FOUND)
+        return resp
+
+    return templates.TemplateResponse("inicio/login.html", {"request": request,
+                                                         "title": "Login"}, status_code=status.HTTP_401_UNAUTHORIZED)
+    
+
+@app.get("/home",
+         tags=["Usuario"],
+         summary="Muestra el inicio de la aplicación.")
+def root(request: Request):
+    return templates.TemplateResponse("usuario/home.html", {"request": request, "title": "Home"})
 
 
 
@@ -179,7 +215,9 @@ def get_register(request: Request):
 def create_registro(request:Request):
     return templates.TemplateResponse("create.html", {"request":request, "title": "Create Reciclaje"})
 
-@app.post("/reciclaje")
+@app.post("/reciclaje", 
+          tags=["Reciclaje"],
+          summary="Registra el reciclaje")
 async def register(request: Request,
              usuario:str=Form(...),
              cantidad:int=Form(...),
@@ -207,8 +245,8 @@ async def register(request: Request,
 @app.get(path="/reciclajes",
          response_class=HTMLResponse,
          response_model=schemas.Reciclaje,
-         tags=["Get"],
-         summary="List all cars",
+         tags=["Reciclaje"],
+         summary="Lista reciclaje almacenado",
          status_code=status.HTTP_200_OK
          )
 def get_reciclajes(request: Request, db: Session = Depends(db.get_db)):
@@ -229,9 +267,36 @@ def get_users(db:Session=Depends(db.get_db), user:schemas.User=Depends(db.get_db
     result=crud.get_users(db=db)
     return jsonable_encoder(result)
 
+@app.get("/perfil",
+         tags=["Usuario"],
+         summary="Busca un perfil.")
+def root(request: Request):
+    return templates.TemplateResponse("buscar-perfil.html", {"request": request, "title": "Perfil"})
+
+
+@app.post("/search-user", response_class=RedirectResponse)
+def search_cars(username: str = Form(...)):
+    return RedirectResponse("/perfil/" + username, status_code=status.HTTP_302_FOUND)
+
+
+
+@app.get('/perfil/{username}',
+         response_class=HTMLResponse,
+         status_code=status.HTTP_200_OK,
+         tags=["Usuario"],
+         response_model=schemas.User,
+         summary="Muestra el prefil de un usuario")
+def perfil_usuario(request: Request, username: str, db: Session = Depends(db.get_db)):
+    usuario = jsonable_encoder(crud.get_user_by_username(db, username))
+    response = templates.TemplateResponse("perfil.html", {"request": request, "usuario": usuario, "title": f"Perfil | {username}"})
+    if not usuario:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return response
+
+
+
 models.Base.metadata.create_all(engine)
     
-
 
 
 
